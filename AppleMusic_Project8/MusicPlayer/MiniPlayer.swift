2b22b0bf09bab5c4c8827eddf18470ce574086d8
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2021-11-01 14:46:27 +0100

    Configuring TabBar().

---
+++ AppleMusic_Project8/MusicPlayer/MiniPlayer.swift
@@ -0,0 +1,62 @@
+//
+//  MiniPlayer.swift
+//  AppleMusic_Project8
+//
+//  Created by Wizzard Sklyarov on 2021. 11. 01..
+//
+
+import SwiftUI
+
+struct MiniPlayer: View {
+    @State var showDetails = false
+    
+    var body: some View {
+        HStack {
+            Image("Picture0")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .scaledToFit()
+            Text("Lady Gaga - Joanne")
+            Spacer()
+            
+            Button(action: {
+                self.showDetails.toggle()
+            }, label: {
+                Image(systemName: "play.fill")
+                    .resizable()
+                    .foregroundColor(.black)
+                    .frame(width: 18, height: 18)
+                    .scaledToFit()
+            })
+            
+            Spacer()
+                .frame(width: 13)
+            
+            Button(action: {
+                self.showDetails.toggle()
+            }, label: {
+                Image(systemName: "forward.fill")
+                    .resizable()
+                    .foregroundColor(.black)
+                    .frame(width: 25, height: 20)
+                    .scaledToFit()
+            })
+                .padding(5)
+        }
+        .padding()
+        .frame(height: 70)
+        .background(
+            VStack(spacing: 0) {
+                BlurView()
+                Divider()
+            })
+        .offset(y: -48)
+    }
+}
+
+
+struct MiniPlayer_Previews: PreviewProvider {
+    static var previews: some View {
+        MiniPlayer()
+    }
+}
