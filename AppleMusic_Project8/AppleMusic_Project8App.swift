e3c53906492a3a349c8a66b5b97b48036c17d53e
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2021-11-01 14:13:27 +0100

    Initial Commit

---
+++ AppleMusic_Project8/AppleMusic_Project8App.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_Project8App.swift
+//  AppleMusic_Project8
+//
+//  Created by Wizzard Sklyarov on 2021. 11. 01..
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_Project8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMusic_Project8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMusic_Project8
+//
+//  Created by Wizzard Sklyarov on 2021. 11. 01..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
